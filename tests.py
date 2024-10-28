e6bdbf01b1c6d1369e5714c0d5b595df0839a606
Merlinux-source <master@merlinux.xyz>
2024-10-28 10:29:05 +0100

    add useless tests

---
+++ tests.py
@@ -0,0 +1,19 @@
+import unittest
+from Maze_class import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(x1=0, y1=0, num_rows=num_rows, num_cols=num_cols, cell_size_x=10, cell_size_y=10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+if __name__ == "__main__":
+    unittest.main()
