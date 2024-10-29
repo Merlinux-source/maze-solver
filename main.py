6047c1f6c7db9ec294134d6e154c8fe9ee4542c4
Merlinux-source <master@merlinux.xyz>
2024-10-29 15:43:01 +0100

    make main.py production ready

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Point_class import Point
 from Line_class import Line
 from Cell_class import Cell
 from Maze_class import Maze
+from time import time
+
 def __main__():
-	win = Window(800, 600)
-	maze = Maze(win, x1=20, y1=20, cell_size_x=25, cell_size_y=25)
-	maze.grid[0][0].draw_move(maze.grid[0][1])
-	maze.grid[0][0].draw_move(maze.grid[1][0])
+	win = Window(400, 300)
+	maze = Maze(win, x1=20, y1=20, cell_size_x=25, cell_size_y=25, entropy=time())
 	win.wait_for_close()
 
 if __name__ == "__main__":
